2035da28ef7a87fcf80d95a5126ede4a7557c012
nayra.adel <nayra.adel@fixedmea.com>
2021-10-22 17:25:22 +0200

    Fetching a Single Review Record

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 export default function ReviewDetailsReviewDetails() {
+  const { id } = useParams()
+  const { loading, error, data } = useFetch('http://localhost:3000/reviews/' + id)
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error...</p>
+
   return (
-    <div>
-    Review Details
+    <div className = "review-card">
+      <div className = "rating"> {data.rating} </div>
+      <h2> {data.title} </h2>
+      <p> {data.body} </p>
     </div>
   )
 }
