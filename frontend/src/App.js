67f540a809a25ff039b13bcb7eb01e5ae31a5adf
Nayra-Adel <nayra.adel@fixedmea.com>
2021-10-22 17:53:09 +0200

    Apollo Client Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 import HomePage from './pages/HomePage'
 import ReviewDetails from './pages/ReviewDetails'
 import Category from './pages/Category'
 import SiteHeader from './components/SiteHeader'
 
+// apollo client
+const client = new ApolloClient({
+  url: 'htttp://localhost:2000//graphql',
+  cache: new InMemoryCache()
+})
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <SiteHeader />
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/details/:id">
-            <ReviewDetails />
-          </Route>
-          <Route path="/category/:id">
-            <Category />
-          </Route>
-        </Switch>
-      </div>
+      <ApolloProvider client = {client}>
+        <div className="App">
+          <SiteHeader />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/details/:id">
+              <ReviewDetails />
+            </Route>
+            <Route path="/category/:id">
+              <Category />
+            </Route>
+          </Switch>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
